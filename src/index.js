79830fe7af7cac0dc08d767976230058c25749e9
Nikk55 <nikhivarshneyaligarh@gmail.com>
2025-11-10 17:03:43 +0530

    Fix encryption

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import dotenv from "dotenv";
 dotenv.config();
-import dotenv from 'dotenv';
+// import dotenv from 'dotenv';
 import express from 'express';
 import cors from 'cors';
 import authRoutes from './routes/auth.js';
